b90f92740fa568f6ce04e8e4da3d9712a50f27dd
Mia Wenjin Zhang <wenjinz@andrew.cmu.edu>
2019-10-01 01:29:59 -0400

    BM25 and BM25 #sum

--- SearchEngine_Fall19/QryEval/QrySopSum.java
+++ SearchEngine_Fall19/QryEval/QrySopSum.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.io.IOException;
 
-public class QrySopSum {
+public class QrySopSum extends QrySop {
+    @Override
+    public double getScore(RetrievalModel r) throws IOException {
+        if (r instanceof RetrievalModelBM25) {
+            return this.getScoreBM25(r);
+        } else {
+            throw new IllegalArgumentException("#SUM cannot be applied to " + r.getClass().getName());
+        }
+    }
+
+    private double getScoreBM25(RetrievalModel r) throws IOException {
+        double score = 0.0;
+        if (this.docIteratorHasMatchCache()) {
+            int doc_id = this.docIteratorGetMatch();
+            for (Qry q_i : this.args) {
+                if (q_i.docIteratorHasMatchCache() && q_i.docIteratorGetMatch() == doc_id) {
+                    score += ((QrySop) q_i).getScore(r);
+                } else {
+                    score += ((QrySop) q_i).getDefaultScore(r, doc_id);
+                }
+            }
+        }
+        return score;
+    }
+
+    @Override
+    public double getDefaultScore(RetrievalModel r, int doc_id) throws IOException {
+        //to-check
+        double score = 0.0;
+        for (Qry q_i : this.args) {
+            score += ((QrySop) q_i).getDefaultScore(r, doc_id);
+        }
+        return score;
+    }
+
+    @Override
+    public boolean docIteratorHasMatch(RetrievalModel r) {
+        return this.docIteratorHasMatchMin(r);
+    }
 }
--- SearchEngine_Fall19/QryEval/RetrievalModelBM25.java
+++ SearchEngine_Fall19/QryEval/RetrievalModelBM25.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+public class RetrievalModelBM25 extends RetrievalModel{
 
-public class RetrievalModelBM25 {
+    private double k1;
+    private double k3;
+    private double b;
+
+    public RetrievalModelBM25(double k1, double k3, double b) {
+        this.k1 = k1;
+        this.k3 = k3;
+        this.b = b;
+    }
+
+    public double getK1() {
+        return k1;
+    }
+
+    public double getK3() {
+        return k3;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    @Override
+    public String defaultQrySopName() {
+        return new String ("#sum");
+    }
 }
